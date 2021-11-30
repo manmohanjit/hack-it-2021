392dae6d30e064b3fda8bfcea32e13ffee389456
Manmohanjit Singh <manmohanjit1@gmail.com>
2021-11-30 15:07:46 +0800

    Add error handling

---
+++ src/main/java/com/cinema/booking/errors/ApiError.java
@@ -0,0 +1,37 @@
+package com.cinema.booking.errors;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+@Setter
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+
+    public ApiError(HttpStatus status, String message, List<String> errors) {
+        super();
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public ApiError(HttpStatus status, String message, String error) {
+        super();
+        this.status = status;
+        this.message = message;
+        errors = List.of(error);
+    }
+
+    public ApiError(HttpStatus status, String message) {
+        super();
+        this.status = status;
+        this.message = message;
+    }
+}
